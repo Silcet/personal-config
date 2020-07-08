122de71c0de075c2c0ff41e7c83d6eb0ed900ac6
cmg <cmg@mir-robots.com>
2020-07-08 13:34:50 +0200

    Pushed local changes

--- fish/functions/mirpyter.fish
+++ fish/functions/mirpyter.fish
@@ -1,11 +1,9 @@
 function mirpyter
     if test -z $argv[1] 
-        #docker run --rm  -p 8888:8888 -e LOCAL_USER_ID=1000 -v /usr/local/mir/software:/usr/local/mir/software -v (pwd):/workspace -it mirrobots-docker.jfrog.io/mir_xenial_jupyter lab
-        docker run --rm  -name "mirpyter" -d -p 8888:8888 -e LOCAL_USER_ID=1000 -v /usr/local/mir/software:/usr/local/mir/software -v (pwd):/workspace -it mir_xenial_jupyter lab
+        docker run --rm  --name "mirpyter" -d -p 8888:8888 -e LOCAL_USER_ID=1000 -v /usr/local/mir/software:/usr/local/mir/software -v (pwd):/workspace -it mir_xenial_jupyter lab
         xdg-open "http://localhost:8888"
     else
-        #docker run --rm  -p 8888:8888 -e LOCAL_USER_ID=1000 -v /usr/local/mir/software:/usr/local/mir/software -v "$argv[1]":/workspace -it mirrobots-docker.jfrog.io/mir_xenial_jupyter lab
-        docker run --rm -name "mirpyter" -d -p 8888:8888 -e LOCAL_USER_ID=1000 -v /usr/local/mir/software:/usr/local/mir/software -v "$argv[1]":/workspace -it mir_xenial_jupyter labi
+        docker run --rm --name "mirpyter" -d -p 8888:8888 -e LOCAL_USER_ID=1000 -v /usr/local/mir/software:/usr/local/mir/software -v "$argv[1]":/workspace -it mir_xenial_jupyter labi
         xdg-open "http://localhost:8888"
     end
 end
